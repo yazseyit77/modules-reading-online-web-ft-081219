4f9d4a423d69b527221e21e0ea5750383b40df22
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-01 03:35:38 +0000

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,13 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
