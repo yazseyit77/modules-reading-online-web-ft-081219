40c0acca8d9b1e40064d252866148bcc94c346ae
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-01 03:28:12 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -16,7 +16,7 @@ module FancyDance
       "Thank you, thank you. It was a pleasure to dance for you all."
     end
   end
-  
+
   module MetaDancing
     def metadata
       "This class produces objects that love to dance."
